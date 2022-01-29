39572292233e075c0efd6bb0775ee3e6e322f549
Rafael <rafa.jo.souza@hotmail.com>
2022-01-29 12:11:46 -0300

    Mapeamento OneToOne Pedido x Pagamento

--- src/main/java/com/panambystudio/cursomc/domain/Pagamento.java
+++ src/main/java/com/panambystudio/cursomc/domain/Pagamento.java
@@ -3,14 +3,25 @@ package com.panambystudio.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
 import com.panambystudio.cursomc.enums.EstadoPagamento;
 
+@Entity
 public class Pagamento implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
 	private Integer id;
 	private EstadoPagamento estado;
 	
+	@OneToOne
+	@JoinColumn(name="pedido_id")
+	@MapsId
 	private Pedido pedido;
 	
 	public Pagamento() {
