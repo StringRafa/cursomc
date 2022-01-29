a212665bba5245913fc947788abc26b17be5b3a0
Rafael <rafa.jo.souza@hotmail.com>
2022-01-29 12:01:33 -0300

    Pagamento, PagamentoComBoleto, PagamentoComCartao

---
+++ src/main/java/com/panambystudio/cursomc/domain/Pedido.java
@@ -0,0 +1,98 @@
+package com.panambystudio.cursomc.domain;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Pedido {
+
+	private Integer id;
+	private Date instante;
+	
+	private Pagamento pagamento;
+	
+	private Cliente cliente;
+	
+	private Endereco enderecoDeEntrega;
+	
+	private Produto itens;
+	
+	public Pedido() {
+	}
+
+	public Pedido(Integer id, Date instante, Pagamento pagamento, Cliente cliente, Endereco enderecoDeEntrega,
+			Produto itens) {
+		super();
+		this.id = id;
+		this.instante = instante;
+		this.pagamento = pagamento;
+		this.cliente = cliente;
+		this.enderecoDeEntrega = enderecoDeEntrega;
+		this.itens = itens;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getInstante() {
+		return instante;
+	}
+
+	public void setInstante(Date instante) {
+		this.instante = instante;
+	}
+
+	public Pagamento getPagamento() {
+		return pagamento;
+	}
+
+	public void setPagamento(Pagamento pagamento) {
+		this.pagamento = pagamento;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Endereco getEnderecoDeEntrega() {
+		return enderecoDeEntrega;
+	}
+
+	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
+		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public Produto getItens() {
+		return itens;
+	}
+
+	public void setItens(Produto itens) {
+		this.itens = itens;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
---
+++ src/main/java/com/panambystudio/cursomc/enums/EstadoPagamento.java
@@ -0,0 +1,36 @@
+package com.panambystudio.cursomc.enums;
+
+public enum EstadoPagamento {
+
+	PENDENTE(1, "Pagamento pendente"),
+	QUITADO(2, "Pagamento quitado"), 
+	CANCELADO(3, "Pagamento cancelado");
+
+	private int cod;
+	private String descricao;
+
+	private EstadoPagamento(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public static EstadoPagamento toEnum(Integer cod) {
+		if (cod == null) {
+			return null;
+		}
+		for (EstadoPagamento x : EstadoPagamento.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+}
