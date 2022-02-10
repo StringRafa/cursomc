ba8609c1fd1eee72a72e578116480b8cfd4aa47c
Rafael <rafa.jo.souza@hotmail.com>
2022-02-10 16:45:25 -0300

    Configuracao inicial do Spring Security

--- src/main/java/com/panambystudio/cursomc/config/TestConfig.java
+++ src/main/java/com/panambystudio/cursomc/config/TestConfig.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.panambystudio.cursomc.services.DBService;
 import com.panambystudio.cursomc.services.EmailService;
@@ -17,15 +19,20 @@ public class TestConfig {
 
 	@Autowired
 	private DBService dbService;
-	
+
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
 		dbService.instantiateTestDatabase();
 		return true;
 	}
-	
+
 	@Bean
 	public EmailService emailService() {
 		return new MockEmailService();
+	}
+
+	@Bean
+	public JavaMailSender javaMailSender() {
+		return new JavaMailSenderImpl();
 	}
 }
--- src/main/java/com/panambystudio/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/panambystudio/cursomc/services/AbstractEmailService.java
@@ -7,6 +7,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
