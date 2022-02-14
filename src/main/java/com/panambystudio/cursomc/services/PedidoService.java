3ca0f21853d1509370c9b8b1c847bf3a476b7894
Rafael <rafa.jo.souza@hotmail.com>
2022-02-14 13:03:55 -0300

    Restricao de conteudo: cliente recupera seus pedido

--- src/main/java/com/panambystudio/cursomc/services/PedidoService.java
+++ src/main/java/com/panambystudio/cursomc/services/PedidoService.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.panambystudio.cursomc.domain.Cliente;
 import com.panambystudio.cursomc.domain.ItemPedido;
 import com.panambystudio.cursomc.domain.PagamentoComBoleto;
 import com.panambystudio.cursomc.domain.Pedido;
@@ -15,6 +19,8 @@ import com.panambystudio.cursomc.enums.EstadoPagamento;
 import com.panambystudio.cursomc.repositories.ItemPedidoRepository;
 import com.panambystudio.cursomc.repositories.PagamentoRepository;
 import com.panambystudio.cursomc.repositories.PedidoRepository;
+import com.panambystudio.cursomc.security.UserSS;
+import com.panambystudio.cursomc.services.exceptions.AuthorizationException;
 import com.panambystudio.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -71,4 +77,15 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
+	}
+	
 }
