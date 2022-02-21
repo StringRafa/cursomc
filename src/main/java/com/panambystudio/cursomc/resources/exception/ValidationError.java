12e0087d7776051331e4dc67b7a4f7946b8db625
Rafael <rafa.jo.souza@hotmail.com>
2022-02-21 10:08:17 -0300

    Padronizando formato de excecoes

--- src/main/java/com/panambystudio/cursomc/resources/exception/ValidationError.java
+++ src/main/java/com/panambystudio/cursomc/resources/exception/ValidationError.java
@@ -3,14 +3,13 @@ package com.panambystudio.cursomc.resources.exception;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ValidationError extends StandardError{
+public class ValidationError extends StandardError {
 	private static final long serialVersionUID = 1L;
 
 	private List<FieldMessage> errors = new ArrayList<>();
-	
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
-		
+
+	public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+		super(timestamp, status, error, message, path);
 	}
 
 	public List<FieldMessage> getErrors() {
