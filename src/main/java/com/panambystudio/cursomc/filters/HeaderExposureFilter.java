28a5378bd7fbb07f803f73afd8fb6936a27db743
Rafael <rafa.jo.souza@hotmail.com>
2022-03-14 08:44:18 -0300

    application.properties

--- src/main/java/com/panambystudio/cursomc/filters/HeaderExposureFilter.java
+++ src/main/java/com/panambystudio/cursomc/filters/HeaderExposureFilter.java
@@ -18,7 +18,7 @@ public class HeaderExposureFilter implements Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletResponse res = (HttpServletResponse) response;
-		res.addHeader("acess-control-expose-headers", "location");
+		res.addHeader("access-control-expose-headers", "location");
 		chain.doFilter(request, response);
 	}
 
